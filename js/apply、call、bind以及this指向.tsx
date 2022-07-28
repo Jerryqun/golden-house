b0793bf0a4fc0d861593e88c64ea3d4dded0d4e7
Jerryqun <932798303@qq.com>
2022-07-28 20:34:15 +0800

    手写apply bind apply

--- js/apply、call、bind以及this指向.tsx
+++ js/apply、call、bind以及this指向.tsx
@@ -9,3 +9,73 @@
  * 3、使用 apply、call、bind
  * 4、new 实例化一个对象
  */
+
+
+window.a = 'window';
+
+let target = {
+    a: 'target'
+}
+
+let test = {
+    a: 'objtest',
+    fn: function (params) {
+        console.log('params', params)
+        console.log('a', this.a)
+        return { a }
+    }
+}
+
+/**手写call */
+
+Function.prototype.my_call = function (context = window, ...args) {
+    if (this === Function.prototype) {
+        return undefined; // 用于防止 Function.prototype.my_call() 直接调用
+    }
+    const fn = Symbol
+    context[fn] = this
+    const res = context.fn(...args)
+    delete context[fn]
+    return res
+}
+
+test.fn.my_apply(target, 1, 2, 3)
+
+/**手写apply */
+
+Function.prototype.my_apply = function (context = window, args) {
+    if (this === Function.prototype) {
+        return undefined; // 用于防止 Function.prototype.my_apply() 直接调用
+    }
+    const fn = Symbol
+    let res;
+    context[fn] = this
+
+    if (Array.isArray(args)) {
+        res = context[fn](...args);
+    } else {
+        res = context[fn]();
+    }
+    delete context[fn]
+    return res
+}
+
+test.fn.my_apply(target, [1, 2, 3])
+
+/**手写bind */
+
+Function.prototype.my_bind = function (context, ...args1) {
+    if (this === Function.prototype) {
+      throw new TypeError('Error')
+    }
+    const _this = this
+    return function fn(...args2) {
+      // 判断是否用于构造函数
+      if (this instanceof fn) {
+        return new _this(...args1, ...args2)
+      }
+      return _this.apply(context, args1.concat(args2))
+    }
+  }
+
+
