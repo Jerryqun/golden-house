287b514442004ac39bd487a9c6edc52e6348281a
Jerryqun <932798303@qq.com>
2022-08-01 21:33:31 +0800

    深拷贝

--- js/手写深拷贝.tsx
+++ js/手写深拷贝.tsx
@@ -1,18 +1,23 @@
 /**
- * 递归 如果遇到循环引用会出现死循环
  * @param target 
  * @param result 
  * @returns 
  */
 
-function deepClone(target, result = {}) {
-    if(Object.prototype.toString.call(target)!== '[object Object]') {
-        return null
+function deepClone(target, map = new WeakMap(), result = {}) {
+    // 克隆原始类型 直接返回
+    if (Object.prototype.toString.call(target) !== '[object Object]') {
+        return target
     }
+    // 防止循环引用
+    if (map.get(target)) {
+        return target
+    }
+    map.set(target, result)
     Object.keys(target).map(key => {
         result[key] = target[key]
         if (!Object.keys(target[key]).length) {
-            deepClone(result[key], result[key])
+            deepClone(result[key], map, result[key])
         }
     })
     return result
