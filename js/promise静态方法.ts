ba11832e8812ba4ad6c267e74ee4ac08f82c751f
Jerryqun <932798303@qq.com>
2022-08-09 20:54:06 +0800

    fix

--- js/promise静态方法.ts
+++ js/promise静态方法.ts
@@ -14,9 +14,9 @@ Promise.myAll = function (value) {
   return new Promise((re, rj) => {
     let count = 0;
     let result = []
-    for (let i = 0; i < value; i++) {
-      re(value[i]).then(res => {
-        count += 1
+    for (let i = 0; i < value.length; i++) {
+      Promise.resolve(value[i]).then(res => {
+        count++;
         result[i] = res
         if (count === value.length) {
           re(result)
@@ -26,3 +26,10 @@ Promise.myAll = function (value) {
     
   })
 }
+
+
+let p1 = new Promise(r => r(2))
+
+let p2 = new Promise(r => r(3))
+
+Promise.myAll([p1,p2]).then(console.log)
