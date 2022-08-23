544037c6cecea521c3fc063f53f1c644e893bd39
Jerryqun <932798303@qq.com>
2022-08-23 20:11:25 +0800

    知识积累

---
+++ js/dom相关.tsx
@@ -0,0 +1,10 @@
+/**
+ * 元素的实际高度
+ * document.getElementById("div").offsetHeight
+ * 元素的实际宽度
+ * document.getElementById("div").offsetWidth
+ * 元素距离左边的距离
+ * document.getElementById("div").offsetLeft
+ * 元素距离上边的距离
+ * document.getElementById("div").offsetTop
+ */
---
+++ react/useLayoutEffect.md
@@ -0,0 +1,2 @@
+useLayoutEffect 与 useEffect Hooks 类似，都是执行副作用操作。但是它是在所有 DOM 更新完成后触发。可以用来执行一些与布局相关的副作用，比如获取 DOM 元素宽高，窗口滚动距离等等。
+
