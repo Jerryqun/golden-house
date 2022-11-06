1c831290ad9039276db6c4494378eeb225ac0777
Jerryqun <932798303@qq.com>
2022-11-06 19:16:54 +0800

    1106

--- js/循环.ts
+++ js/循环.ts
@@ -29,11 +29,11 @@ list.forEach((item) => {
   item.count = 3;
 });
 
-console.log(list); // 引用类型如果有count属性则直接改变 ，没有则新家一个count属性
+console.log(list); // 引用类型如果有count属性则直接改变 ，没有则新加一个count属性
 
 /**
  * for 循环 和 for in 循环
- *  for in 循环数组吐出的是index  循环对象吐出的是key
+ * for in 循环数组吐出的是index  循环对象吐出的是key
  * break 会跳出循环
  * continue 会跳出当前次循环（continue后面的代码不会执行）
  */
@@ -56,7 +56,7 @@ for (let item of list) {
 /**
  * for in 和 for of
  * 两者区别：
-    1.for-in只是获取数组的索引；而for-of会获取数组的值
+    1.for-in只是获取数组的索引或者对象的key；而for-of会获取数组的值
     2.for-in会遍历对象的整个原型链，性能差；而for-of只遍历当前对象，不会遍历原型链
     3.对于数组的遍历，for-in会返回数组中所有可枚举的属性(包括原型链上可枚举的属性)；for-of只返回数组的下标对应的属性值
     4.for-of适用遍历数组／字符串/map/set等有迭代器对象的集合，但是不能遍历普通对象（obj is not iterable）
---
+++ 数据结构/JS中7种数据类型.md
@@ -0,0 +1,8 @@
+基本类型：String、Number、Undefined、Booleam、Symbol、Null
+引用类型：Object
+
+二者有什么区别
+1、变量保存基本类型数据时，是直接栈内保存的，而引用类型是在堆中生成，在栈内保存的是该对象的引用
+2、基本类型数据本身不可变，但引用类型数据是可变的
+
+undefined 表示变量未初始化，null 表示变量为空
